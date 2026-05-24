81cd1b38f50df7642813c07d6b8a00cb86e98b83
qweq05293 <qweq05293@gmail.com>
2026-05-24 08:24:44 +0000

    add cloud tunel

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -1,6 +1,6 @@
-import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
@@ -10,5 +10,9 @@ export default defineConfig({
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+ server: {
+    host: "0.0.0.0",
+    allowedHosts: true
   },
 });
