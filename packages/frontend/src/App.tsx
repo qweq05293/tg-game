94932211278cbd289e6c576f68609fba4a0fc8fa
qweq05293 <qweq05293@gmail.com>
2026-05-25 18:20:25 +0000

    fix : ready arval types

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,8 +1,9 @@
 import { useEffect, useState } from "react";
 import { telegramLogin } from "./lib/auth";
+import type { TelegramLoginResponseDto } from "./shared/api";
 
 export function App() {
-  const [data, setData] = useState(null);
+  const [data, setData] = useState<TelegramLoginResponseDto | null>(null);
 
   useEffect(() => {
     telegramLogin().then((loginData) => {
@@ -12,7 +13,7 @@ export function App() {
   if (!data) {
     return <div>Loading...</div>;
   }
-  return <div>{JSON.stringify(data , null , 2)}</div>;
+  return <div>{JSON.stringify(data, null, 2)}</div>;
 }
 
 export default App;
